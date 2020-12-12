824dc1421903c5b5d79073f00c4e2064873d7daa
Angela Sun <angelasun1234@gmail.com>
2020-12-12 13:56:54 -0500

    randomly generate GIPHY on home page

--- src/App.js
+++ src/App.js
@@ -19,54 +19,16 @@ import HighProtein from "./HighProtein.js";
 
 import {Navbar,Nav, NavDropdown} from 'react-bootstrap';
 class App extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      randGif: [],
-    };
-    this.componentDidMount = this.componentDidMount.bind(this);
-  }
-
-  componentDidMount() {
-
-  }
-  randGif(){
-    var gif0 = <iframe src="https://giphy.com/embed/b5Hcaz7EPz26I" width="480" height="357" frameBorder="1" ></iframe>;
-    var gif1 = <iframe src="https://giphy.com/embed/N23cG6apipMmQ" width="480" height="342" frameBorder="1"></iframe>
-    var gif2 = <iframe src="https://giphy.com/embed/CNocEFcF9IBegtgW3q" width="480" height="360" frameBorder="1"></iframe>
-    var gif3 = <iframe src="https://giphy.com/embed/rkgX9MTBXJa1O" width="480" height="360" frameBorder="1"></iframe>
-    var gif4 = <iframe src="https://giphy.com/embed/10sTpXkrooA2bK" width="480" height="357" frameBorder="1"></iframe>
-    var randVal= Math.floor(Math.random);
-    if(randVal % 5 === 0){
-      console.log("randVal: " + 0);
-      this.setState({
-        randGif: gif0
-      })
-    }else if(randVal % 5 === 1){
-      console.log("randVal: " + 1);
-      this.setState({
-        randGif: gif1
-      })   
-     }else if(randVal % 5 === 2){
-      console.log("randVal: " + 2);
-      this.setState({
-        randGif: gif2
-      }) 
-    }else if(randVal % 5 === 3){
-      console.log("randVal: " + 3);
-      this.setState({
-        randGif: gif3
-      }) 
-    }else if(randVal % 5 === 4){
-      console.log("randVal: " + 4);
-      this.setState({
-        randGif: gif4
-      }) 
-    }
-  }
-
 
   render(){
+    var gif = [];
+    gif[0] = <iframe src="https://giphy.com/embed/b5Hcaz7EPz26I" width="480" height="357" frameBorder="0" ></iframe>;
+    gif[1] = <iframe src="https://giphy.com/embed/N23cG6apipMmQ" width="480" height="342" frameBorder="0"></iframe>
+    gif[2] = <iframe src="https://giphy.com/embed/CNocEFcF9IBegtgW3q" width="480" height="360" frameBorder="0"></iframe>
+    gif[3] = <iframe src="https://giphy.com/embed/rkgX9MTBXJa1O" width="480" height="360" frameBorder="0"></iframe>
+    gif[4] = <iframe src="https://giphy.com/embed/10sTpXkrooA2bK" width="480" height="357" frameBorder="0"></iframe>
+    var randVal= Math.floor(Math.random() * 10);
+    var randIndex = randVal % 5;
     return (
       <div>
   <Navbar bg="primary" variant="dark" sticky="top" >
@@ -104,9 +66,10 @@ class App extends React.Component{
             <Route exact path="/lowsugar" component={LowSugar}/>
             <Route exact path="/highpro" component={HighProtein}/>
             <Route exact path="/home">
+            <h3>Welcome to our recipes app! Happy cooking :)</h3>
             <div className="results-container" id="results">
-              <h3>Welcome to our recipes app! Happy cooking :)</h3>
-              {this.state.randGif}
+              <p></p>
+              {gif[randIndex]}
             </div>
             </Route>
           </div>
